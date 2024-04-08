1de36e2c1c6bc4061089b7fe597caa0e4e323388
mariamawit seifu <merrybee34@gmail.com>
2024-04-07 23:07:59 -0700

    serverjs

--- server.js
+++ server.js
@@ -26,23 +26,26 @@
 // });
 
 
-
 const express = require('express');
 const next = require('next');
+const http = require('http');
 
 const dev = process.env.NODE_ENV !== 'production';
 const app = next({ dev });
 const handle = app.getRequestHandler();
 
-app.prepare().then(() => {
-  const server = express();
+const port = process.env.PORT || 3000;
+const server = express();
 
+app.prepare().then(() => {
   server.all('*', (req, res) => {
     return handle(req, res);
   });
 
-  server.listen(3000, (err) => {
+  const httpServer = http.createServer(server);
+
+  httpServer.listen(port, (err) => {
     if (err) throw err;
-    console.log('> Ready on http://localhost:3000');
+    console.log(`> Ready on http://localhost:${port}`);
   });
 });
