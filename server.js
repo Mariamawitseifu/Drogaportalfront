07611c96bc78c62bf5f83b7ebc561c28f0f129ad
mariamawit seifu <merrybee34@gmail.com>
2024-04-08 00:14:44 -0700

    serverjs

--- server.js
+++ server.js
@@ -1,51 +1,37 @@
-// const { createServer } = require('http');
-// const { parse } = require('url');
-// const next = require('next');
-
-// const dev = process.env.NODE_ENV !== 'production';
-// const app = next({ dev });
-// const handle = app.getRequestHandler();
-// const port = process.env.PORT || 3000;
-
-// app.prepare().then(() => {
-//     createServer((req, res) => {
-//         const parsedUrl = parse(req.url, true);
-//         const { pathname, query } = parsedUrl;
-
-//         if (pathname === '/page.js') {
-//             app.render(req, res, '/page.js', query);
-//         } else if (pathname === '/page.js') {
-//             app.render(req, res, '/page.js', query);
-//         } else {
-//             handle(req, res, parsedUrl);
-//         }
-//     }).listen(port, (err) => {
-//         if (err) throw err;
-//         console.log(`> Ready on http://localhost:${port}`);
-//     });
-// });
-
-
-const express = require('express');
+const { createServer } = require('http');
+const { parse } = require('url');
 const next = require('next');
-const http = require('http');
 
 const dev = process.env.NODE_ENV !== 'production';
-const app = next({ dev });
+const hostname =  'localhost'
+const port = process.env.port || 3000
+const app = next({ dev, hostname, port });
 const handle = app.getRequestHandler();
 
-const port = process.env.PORT || 3000;
-const server = express();
-
 app.prepare().then(() => {
-  server.all('*', (req, res) => {
-    return handle(req, res);
-  });
+    createServer(async(req, res) => {
+    try{
+        const parsedUrl = parse(req.url, true);
+        const { pathname, query } = parsedUrl;
 
-  const httpServer = http.createServer(server);
-
-  httpServer.listen(port, (err) => {
-    if (err) throw err;
-    console.log(`> Ready on http://localhost:${port}`);
-  });
+        if (pathname === '/a') {
+           await app.render(req, res, '/a', query);
+        } else if (pathname === '/b') {
+           await app.render(req, res, '/b', query);
+        } else {
+           await handle(req, res, parsedUrl);
+        }
+      } catch (err){
+console.error('Error occurred handling', req.url, err)
+res.statusCode = 500
+res.end('internal server error')
+      }
+    })
+    .once('error', (err) => {
+        console.error(err)
+        process.exit(1)
+    })
+    .listen(port, (er) => {
+        console.log(`> Ready on http://localhost:${port}`);
+    });
 });
